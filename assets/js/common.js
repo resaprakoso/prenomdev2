a162aa645425e83ab48d93ce0ce3d0cde0d21818
resaprakoso <rprakosoh@gmail.com>
2025-06-13 08:40:41 +0700

    first commit

---
+++ assets/js/common.js
@@ -0,0 +1,26 @@
+function menuWorks(){
+const element = document.getElementById("scrollWorks")
+const closeNav = document.getElementById("box-menu-mobile")
+element.scrollIntoView();
+closeNav.style.right = "-100%";
+closeNav.style.transition = "0ms";
+}
+
+function menuContact(){
+const element = document.getElementById("scrollContact")
+const closeNav = document.getElementById("box-menu-mobile")
+element.scrollIntoView();
+closeNav.style.right = "-100%";
+closeNav.style.transition = "0ms";
+}
+
+function hamburger(){
+const openNav  = document.getElementById("box-menu-mobile")
+openNav.style.right = "0";
+openNav.style.transition = "300ms"
+}
+
+function closeHamburger(){
+    const closeHamburger = document.getElementById("box-menu-mobile")
+    closeHamburger.style.right = "-100%";
+}
